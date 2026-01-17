58d85d2e8fd06b30f9e5ba6d75467523a398fa24
realmzlegend-collab <realmzlegend@gmail.com>
2026-01-17 15:30:33 +0100

    Delete backend/models/models.template.js

--- backend/models/models.template.js
+++
@@ -1,9 +0,0 @@
-const { mongoose } = require('../lib/mongoose');
-const { Schema } = mongoose;
-
-const TemplateSchema = new Schema({
-  title: String,
-  content: String,
-}, { timestamps: true });
-
-module.exports = mongoose.models?.Template || mongoose.model('Template', TemplateSchema);
